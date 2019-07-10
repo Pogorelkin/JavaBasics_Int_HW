b3b768095a21a25a3647a3d0d37037f27a800a88
Vadim Pogorelkin <vadim_pogorelkin@epam.com>
2019-07-10 14:08:56 +0400

    Update Test runner suite

--- src/test/java/com/epam/TestRunner.java
+++ src/test/java/com/epam/TestRunner.java
@@ -1,4 +1,14 @@
 package com.epam;
 
+import com.epam.calculator.CalculatorUtilityTest;
+import com.epam.calculator.ParserUtilityTest;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({CalculatorUtilityTest.class, ParserUtilityTest.class})
 public class TestRunner {
+
+
+
 }
